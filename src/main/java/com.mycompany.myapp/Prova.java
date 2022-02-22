a35005949eaab8eb8f8760687a8b2fd26e3e4c9d
javierpaniza <javierpaniza>
2022-02-22 20:33:37 +0100

    Initial commit

---
+++ src/main/java/com.mycompany.myapp/Prova.java
@@ -0,0 +1,10 @@
+package com.mycompany.myapp;
+
+import org.openxava.util.Strings;
+
+public class Prova {
+
+    public String getNombre() {
+        return Strings.firstUpper("pedro");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Adios <%=new com.mycompany.myapp.Prova().getNombre()%></h2>
+</body>
+</html>
